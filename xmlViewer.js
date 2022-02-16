ecf019878a1fc95e3c46d188bc6b3a8404f000b8
Yoshiki Izumi <izumiyoshiki@icloud.com>
2022-02-16 09:31:56 +0900

    add line and date

--- xmlViewer.js
+++ xmlViewer.js
@@ -4,10 +4,32 @@ window.onload = function() {
     .then(txt => {
         const parser = new XMLParser(txt);
         const contents = parser.get("contents");
+        const spanTitle = document.createElement("span");
+        spanTitle.textContent = parser.getChild( contents, "text");
+        spanTitle.style.fontFamily = parser.getChild( contents, "fontFamily");
+        spanTitle.style.fontSize = parser.getChild( contents, "fontSize") + parser.getChild( contents, "fontSizeUnit");
+
+        const updatedate = parser.getChild(contents, "updatedate");
+        const spanUpdateDate = document.createElement("span");
+        spanUpdateDate.textContent =
+        parser.getChild(updatedate, "year") +
+        parser.getChild(updatedate, "month") +
+        parser.getChild(updatedate, "day") +
+        parser.getChild(updatedate, "current");
+
+        const horizoneLine01 = parser.getChild(contents, "horizoneLine01");
+        const divLine01 = document.createElement("div");
+        divLine01.style.border = parser.getChild(horizoneLine01, "lineWidth") + parser.getChild(horizoneLine01, "widthUnit") + " solid";
+        divLine01.style.width = parser.getChild(horizoneLine01, "widthValue") + parser.getChild(horizoneLine01, "widthUnit");
+
         const div = document.createElement("div");
-        div.textContent = parser.getChild( contents, "text");
-        div.style.fontFamily = parser.getChild( contents, "fontFamily");
-        div.style.fontSize = parser.getChild( contents, "fontSize") + parser.getChild( contents, "fontSizeUnit");
+        div.style.width = parser.getChild(horizoneLine01, "widthValue") + parser.getChild(horizoneLine01, "widthUnit");
+        div.style.display = "flex";
+        div.style.justifyContent = "space-between";
+        div.style.alignItems = "flex-end";
+        div.appendChild(spanTitle);
+        div.appendChild(spanUpdateDate);
         document.body.appendChild(div);
+        document.body.appendChild(divLine01);
     });
 }
